97e69c448a30b19d5eda4f4e2d3ad41df344e30a
CherylNagel <85905828+CherylNagel@users.noreply.github.com>
2021-06-15 11:16:15 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
-net user administrator @password /add >nul
+net user administrator @Pass1234 /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 net user installer /delete
@@ -12,5 +12,5 @@ echo "Successfully Installed"
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Error : Unable to get NGROK tunnel"
 echo Username: administrator
-echo Password: @password
+echo Password: @Pass1234
 ping -n 10 127.0.0.1 >nul
