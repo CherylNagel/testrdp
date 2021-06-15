498e99205dcb53dca072876c5a34aa382d7a6e35
CherylNagel <85905828+CherylNagel@users.noreply.github.com>
2021-06-15 09:46:45 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -8,9 +8,9 @@ sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
-echo  Successfully Installed !
+echo "Successfully Installed"
 echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Error : Unable to get NGROK tunnel"
 echo Username: administrator
 echo Password: @password
 ping -n 10 127.0.0.1 >nul
